78f3979919906e1fa19c8814b0dcbe239a076e36
Riley Miller <rileymiller@mymail.mines.edu>
2017-08-01 16:36:02 -0600

    Add files via upload
    D3 versioning. Data is bound to points and points are draggable.

--- index.js
+++ index.js
@@ -1,97 +1,159 @@
+var points = [];
+var point_count = 0;
 (function() {
 
-    //this is the event loop for the application
     window.main = function() {
-        var points = [];
-        //need to define the drag behavior of existing points
-        var drag_index;
-        //need to define hover state of nodes
+        /* obtain a reference to the SVG
+         */
+        var background, drag, vis;
+        vis = d3.select('svg');
+        /* --- Circle dragging ---
+         */
+        /* define a drag behavior
+         */
+        function updatePoints(point) {
+            console.log('inside updatePoints');
+            //console.log(point.getAttribute('id'));
+            var id = point.getAttribute('id');
+            console.log(d3.select(point).select('circle').attr('cx'))
+            console.log(d3.select(point).select('circle').attr('cy'))
 
-        /***************************************************************
-        // need to define the creation of new points on click,
-        // upon creation of new point--modal launched prompting for 
-        // information concerning point. Encapsulated in a data structure
-        //*************************************************************/
-        var vis = d3.select('svg');
-        var background;
-        background = vis.append('rect').attr('class', 'background').attr('width', vis.attr('width')).attr('height', vis.attr('height'));
-
-
-        var drag = d3.behavior.drag()
-            .on("drag", function(d, i) {
-                d.x += d3.event.dx
-                d.y += d3.event.dy
-                d3.select(this).attr("transform", function(d, i) {
-                    return "translate(" + [d.x, d.y] + ")"
-                })
-            });
-
-
-
-
-
-        function drawPoints(point) {
-
-            //var coord = [point.x, point.y]l
-            var node = vis.append('svg:g')
-                .data(point)
-                .enter()
-                .attr('class', 'node')
-                .attr('transform', 'translate(' + point.x + ',' + point.y + ')')
-                .call(drag);
-
-
-            var circle = node.selectAll('circle')
-                .data(point)
-                .enter()
-                .attr('class', 'circle')
-                .attr('cx', function(d) { return d.x; })
-                .attr('cy', function(d) { return d.y; })
-                .attr('r', function(d) { return d.radius; })
-                .style('fill', function(d) { return d.color; })
-        }
-
-
-        function intersect(p) {
-            var count = 0;
-            var intersects = false;
-            for (var point in points) {
-
-                if (p[0] == point.x && p[1] == point.y) {
-                    return true;
+            for (var i = 0; i < points.length; i++) {
+                if (points[i].id === id) {
+                    points[i].x = d3.select(point).select('circle').attr('cx');
+                    points[i].y = d3.select(point).select('circle').attr('cy');
                 }
-
-                count++;
             }
+        }
+        /*
+                drag = d3.behavior.drag().on('drag', function() {
+                    /* move the circle
+                     */
+        /*
+                    console.log(this);
+                    //d3.select(this).attr('cx', d3.event.x).attr('cy', d3.event.y);
+                    //d3.select(this).attr()
+                    d3.select(this).select("text")
+                        .attr("x", function(d){return d.x = d3.event.x; })
+                        .attr("y", function(d) { return d.y = d3.event.y; });
+                    d3.select(this).select("circle")
+                        .attr("cx", function(d) {return d.x = d3.event.x;} )
+                        .attr("cy", function(d) { return d.y = d3.event.y; });
+                    updatePoints(this);
+                });*/
+        var drag = d3.behavior.drag()
+            .origin(function(d) { return d; })
+            .on("dragstart", dragstarted)
+            .on("drag", dragged)
+            .on("dragend", dragended);
 
-            return false;
+        function dragstarted(d) {
+            d3.select(this).classed("active", true);
+            
         }
 
-        vis.on('click', function() {
-            var point;
-            p = d3.mouse(this);
-            console.log(p);
+        function dragged(d) {
+            d3.select(this).select("text")
+                .attr("x", function(d) { return d.x = d3.event.x; })
+                .attr("y", function(d) { return d.y = d3.event.y; });
+            d3.select(this).select("circle")
+                .attr("cx", function(d) { return d.x = d3.event.x; })
+                .attr("cy", function(d) { return d.y = d3.event.y; });
+        }
 
-            //  if (!intersect(p)) {
+        function dragended(d) {
+            d3.select(this).classed("active", false);
+            updatePoints(this);
+        }
+        /* --- Circle creation ---
+         */
+        /* create a rectangle to serve as background
+         */
+        background = vis.append('rect').attr('class', 'background').attr('width', vis.attr('width')).attr('height', vis.attr('height'));
+        /* when the user clicks the background
+         */
+        return background.on('click', function() {
+            /* retrieve mouse coordinates
+             */
+            var p;
+            p = d3.mouse(this);
+            /* create a new circle at point p
+             */
+
             r = prompt('what is the radius of the circle? (px)');
             c = prompt('what color is the circle (word or hex)');
             var point = {
                 x: p[0],
                 y: p[1],
                 radius: r,
-                color: c
+                color: c,
+                count: point_count,
+                id: 'node' + point_count
 
             };
-            console.log('clicked point: ' + p);
-            console.log(point);
+            point_count++;
+            console.log(p);
             points.push(point);
-            drawPoints(point);
+            /*return vis.append('circle').data(points).enter()
+            .attr('class', function(d,i) { return 'node' + i;})
+            .attr('r', function(d) { return d.radius})
+            .attr('cx', function(d) { return d.x;})
+            .attr('cy', function(d){ return d.y;}).
+            attr('fill', function(d) { return d.color}).call(drag);*/
+
+            console.log(points);
+            var node = vis.selectAll('g')
+                .data(points)
+                .enter().append('g')
+                .attr('id', function(d, i) { return 'node' + i }).call(drag);
+
+
+            console.log(node.data());
 
 
 
+            node.append('circle')
+                .attr('class', function(d) { return 'circle' + d.count; })
+                .attr('r', function(d) { return d.radius; })
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('fill', function(d) { return d.color; });
 
+            var text = node.append("text")
+                .attr('class', 'start')
+                .attr("x", function(d) { return d.x + d.radius / 2; })
+                .attr("y", function(d) { return d.y + d.radius / 2; })
+                .attr("text-anchor", "start")
+                .style("fill", "black")
+                .text("text-demo");
+
+            console.log(text.data());
         });
+
+
     };
 
-
 }).call(this);
+
+/*
+vis.on('click', function() {
+    var point;
+    p = d3.mouse(this);
+    console.log(p);
+
+    //  if (!intersect(p)) {
+    r = prompt('what is the radius of the circle? (px)');
+    c = prompt('what color is the circle (word or hex)');
+    var point = {
+        x: p[0],
+        y: p[1],
+        radius: r,
+        color: c
+
+    };
+    console.log('clicked point: ' + p);
+    console.log(point);
+    points.push(point);
+    console.log(points);
+    //drawPoints(point);
+});*/
