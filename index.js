4e88508c686e87f9d236c247442a68e17270d763
Riley Miller <rileymiller@mymail.mines.edu>
2017-10-03 23:21:08 -0600

    uploaded working d3 early prototype
    Finally fixed the event binding bug. Canary (d3) branch is now up to speed and working with basic functionality.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var points = [];
 var point_count = 0;
+var p;
 (function() {
 
     window.main = function() {
@@ -25,22 +26,7 @@ var point_count = 0;
                 }
             }
         }
-        /*
-                drag = d3.behavior.drag().on('drag', function() {
-                    /* move the circle
-                     */
-        /*
-                    console.log(this);
-                    //d3.select(this).attr('cx', d3.event.x).attr('cy', d3.event.y);
-                    //d3.select(this).attr()
-                    d3.select(this).select("text")
-                        .attr("x", function(d){return d.x = d3.event.x; })
-                        .attr("y", function(d) { return d.y = d3.event.y; });
-                    d3.select(this).select("circle")
-                        .attr("cx", function(d) {return d.x = d3.event.x;} )
-                        .attr("cy", function(d) { return d.y = d3.event.y; });
-                    updatePoints(this);
-                });*/
+        
         var drag = d3.behavior.drag()
             .origin(function(d) { return d; })
             .on("dragstart", dragstarted)
@@ -48,8 +34,9 @@ var point_count = 0;
             .on("dragend", dragended);
 
         function dragstarted(d) {
+            d3.select(this).classed("spot", false);
             d3.select(this).classed("active", true);
-            
+
         }
 
         function dragged(d) {
@@ -63,6 +50,7 @@ var point_count = 0;
 
         function dragended(d) {
             d3.select(this).classed("active", false);
+            d3.select(this).classed("spot", true);
             updatePoints(this);
         }
         /* --- Circle creation ---
@@ -72,49 +60,81 @@ var point_count = 0;
         background = vis.append('rect').attr('class', 'background').attr('width', vis.attr('width')).attr('height', vis.attr('height'));
         /* when the user clicks the background
          */
-        return background.on('click', function() {
+
+        background.on('click', null);
+        background.on('click', function() {
             /* retrieve mouse coordinates
              */
-            var p;
+            
             p = d3.mouse(this);
+            console.log('background clicked')
+            console.log('these are the coordinates of p at this point in the function');
+            console.log(p);
             /* create a new circle at point p
              */
+            $('#prompt').modal({ show: false });
+            $('#prompt').modal('show');
 
-            r = prompt('what is the radius of the circle? (px)');
-            c = prompt('what color is the circle (word or hex)');
+
+
+            d3.event.stopPropagation();
+           
+        });
+        $('#submit').click(function(e) {
+                // console.log('#submit clicked');
+                //need to capture this data
+                //console.log('submit handled');
+                e.preventDefault();
+                var yrs = $('#number-input').val();
+                //  console.log(yrs);
+                var skills = $('#skillSelect').val();
+                //  console.log(skills);
+                var description = $('#exampleTextarea').val();
+                //console.log(description);
+                var color = $('#colorSelect').val();
+                // console.log(color)
+                if (point_count === points.length) {
+                    console.log('POINT COUNT = POINTS.LENGTH');
+
+                     point_count++;
+                     console.log('point count after increment: ' + point_count);
+                    newPoint(p, yrs, skills, description, color);
+                }
+
+               
+            });
+        function newPoint(p, years, skills, description, color) {
+           
             var point = {
                 x: p[0],
                 y: p[1],
-                radius: r,
-                color: c,
+                radius: years,
+                color: color,
                 count: point_count,
-                id: 'node' + point_count
+                id: 'node' + point_count,
+                yrs: years,
+                skills: skills,
+                description: description
 
             };
-            point_count++;
-            console.log(p);
+            // console.log(p);
             points.push(point);
-            /*return vis.append('circle').data(points).enter()
-            .attr('class', function(d,i) { return 'node' + i;})
-            .attr('r', function(d) { return d.radius})
-            .attr('cx', function(d) { return d.x;})
-            .attr('cy', function(d){ return d.y;}).
-            attr('fill', function(d) { return d.color}).call(drag);*/
-
-            console.log(points);
+            
+            //console.log(points);
             var node = vis.selectAll('g')
                 .data(points)
                 .enter().append('g')
-                .attr('id', function(d, i) { return 'node' + i }).call(drag);
+                .attr('class', function(d) { return 'spot'; })
+                .attr('id', function(d, i) { return 'node-' + i }).call(drag);
 
 
-            console.log(node.data());
+            //console.log(node.data());
 
 
 
             node.append('circle')
-                .attr('class', function(d) { return 'circle' + d.count; })
-                .attr('r', function(d) { return d.radius; })
+                .attr('class', function(d) { return 'circle-' + d.count; })
+                .attr('r', function(d) { return d.radius * 10; })
                 .attr('cx', function(d) { return d.x; })
                 .attr('cy', function(d) { return d.y; })
                 .attr('fill', function(d) { return d.color; });
@@ -125,35 +145,12 @@ var point_count = 0;
                 .attr("y", function(d) { return d.y + d.radius / 2; })
                 .attr("text-anchor", "start")
                 .style("fill", "black")
-                .text("text-demo");
+                .text(function(d) { return d.id; });
 
-            console.log(text.data());
-        });
-
+            // console.log(text.data());
+        }
 
     };
 
 }).call(this);
 
-/*
-vis.on('click', function() {
-    var point;
-    p = d3.mouse(this);
-    console.log(p);
-
-    //  if (!intersect(p)) {
-    r = prompt('what is the radius of the circle? (px)');
-    c = prompt('what color is the circle (word or hex)');
-    var point = {
-        x: p[0],
-        y: p[1],
-        radius: r,
-        color: c
-
-    };
-    console.log('clicked point: ' + p);
-    console.log(point);
-    points.push(point);
-    console.log(points);
-    //drawPoints(point);
-});*/
